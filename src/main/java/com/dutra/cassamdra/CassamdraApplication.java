5f377f92e38f8c7077d0dd6b1b249ba987e511af
David Alves Dutra <dadutra@hotmail.com>
2025-02-12 10:09:25 -0300

    Projeto base adicionado.

---
+++ src/main/java/com/dutra/cassamdra/CassamdraApplication.java
@@ -0,0 +1,13 @@
+package com.dutra.cassamdra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassamdraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassamdraApplication.class, args);
+	}
+
+}
